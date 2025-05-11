5e42168c3c9956fb7a4402eecd1151528d3b1aac
AbdulBaseer-01 <mohammdabdulbaseer8@gmail.com>
2025-05-12 01:32:58 +0530

    Focus card and carousel added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { GlobeDemo } from "@/components/global";
+import { CarouselDemo } from "@/components/Slide";
+import { FocusCardsDemo } from "@/components/focusCards";
 
 export default function Home() {
   return (
-    <GlobeDemo/>
+    <>
+      <GlobeDemo/>
+      <FocusCardsDemo/>
+      <CarouselDemo/>
+    </>
   );
 }
